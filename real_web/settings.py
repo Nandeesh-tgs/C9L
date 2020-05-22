282097fdf52dc704da8eff40675a9686160957b4
Nandeesh-tgs <63971807+Nandeesh-tgs@users.noreply.github.com>
2020-05-23 00:12:44 +0530

    setttings change to server

--- real_web/settings.py
+++ real_web/settings.py
@@ -154,3 +154,8 @@ EMAIL_PORT=587
 EMAIL_HOST_USER='nandeesh.tgsvt@gmail.com'
 EMAIL_HOST_PASSWORD='A2Zhoysala@'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
